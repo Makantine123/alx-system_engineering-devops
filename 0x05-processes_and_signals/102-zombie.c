d7abd6a5ba2a31fd5998d2301b680f265d4ada47
Siphelele makhathini <siphelele.mlungisi@gail.com>
2023-02-28 22:06:18 +0200

    Commit

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+
+/**
+ * infinite_while - loops infinitely
+ *
+ * Return: Always 0 - Success
+ */
+int infinite_while(void)
+{
+while (1)
+{
+sleep(1);
+}
+return (0);
+}
+
+/**
+ * main - Creates Zombie Process then sleeps 2 seconds
+ *
+ * Return: Always 0 - Success
+ */
+int main(void)
+{
+int pid, count;
+
+for (count = 0; count < 5; count++)
+{
+pid = fork();
+if (pid != 0)
+{
+printf("Zombie process created, PID: %d\n", pid);
+}
+else
+return (0);
+}
+infinite_while();
+return (0);
+}
