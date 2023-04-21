b85520971653d84533859195bcba81be15230db9
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-04-21 12:12:09 +0200

    commit

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,9 @@
+pckage { 'python3-pip':
+  ensure => installed,
+}
+
+exec { 'install_flask':
+    command => '/usr/bin/pip3 install flask==2.1.0',
+    path    => ['/usr/bin', '/usr/sbin', '/bin'],
+    creates => '/usr/local/lib/python3.8/dist-packages/flask',
+}
