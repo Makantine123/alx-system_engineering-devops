9683818b1eb969fe0791b79f523340c6de4e5d46
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-05-25 00:17:45 +0200

    Commit

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -10,15 +10,14 @@ from sys import argv
 if __name__ == "__main__":
     userID = argv[1]
     url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "user/{}".format(userID)).json()
+    user = requests.get(url + "users/{}".format(userID)).json()
     todos = requests.get(url + "todos", params={"userId": userID}).json()
-    name = user.get("name")
     completed = []
     for task in todos:
         if task.get("completed") is True:
             completed.append(task.get("title"))
-    print("Employee {} is done with tasks({}/{})".format(name, len(completed),
-                                                         len(todos)))
+    print("Employee {} is done with tasks({}/{})".
+          format(user.get("name"), len(completed), len(todos)))
     for title in completed:
         print("\t{}".format(title))
 
