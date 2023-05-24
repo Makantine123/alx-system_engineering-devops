90ffe900ec8e5457f67aa999c4d7b9a6b9ab3443
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-05-25 00:02:37 +0200

    Commit

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Script uses REST API for a given employee ID, returns information about
+his/her TODO list progress
+"""
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    userID = argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "user/{}".format(userID)).json()
+    todos = requests.get(url + "todos", params={"userId": userID}).json()
+    name = user.get("name")
+    completed = []
+    for task in todos:
+        if task.get("completed") is True:
+            completed.append(task.get("title"))
+    print("Employee {} is done with tasks({}/{})".format(name, len(completed),
+                                                         len(todos)))
+    for title in completed:
+        print("\t{}".format(title))
+
