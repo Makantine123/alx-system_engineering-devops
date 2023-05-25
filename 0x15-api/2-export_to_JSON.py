c8ce26d6a9f9aae38c36cbb74213b79728ccfa46
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-05-25 20:28:39 +0200

    Commit

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -4,31 +4,22 @@ Script to export data in the JSON format
 """
 
 import json
-from os import write
 import requests
 from sys import argv
 
 
 def export_json(userID):
     url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(userID)).json()
     todos = requests.get(url + "todos", params={"userId": userID}).json()
-
+    user = requests.get(url + "users/{}".format(userID)).json()
     file_name = userID + ".json"
-
-    user_data = {str(userID): []}
-
-    for task in todos:
-        user_task = {
-            "task": task["title"],
-            "completed": task["completed"],
-            "username": user["username"],
-        }
-        user_data[userID].append(user_task)
 
     file_name = userID + ".json"
     with open(file_name, "w") as file:
-        json.dump(user_data, file)
+        for t in todos:
+            json.dump({userID: [{"task": t["title"],
+                                 "completed": t["completed"],
+                                 "username": user["username"]}]}, file)
 
 
 if __name__ == "__main__":
