48e2816d43bba8e3216ddc3e8f41e4a64a06b8a0
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-05-25 02:28:21 +0200

    Commit

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -6,18 +6,22 @@ his/her TODO list progress
 
 import requests
 from sys import argv
+import csv
 
 if __name__ == "__main__":
     userID = argv[1]
     url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(userID)).json()
+    user = requests.get(url + "users", params={"id": userID}).json()
     todos = requests.get(url + "todos", params={"userId": userID}).json()
+    name = user["name"]
     completed = []
-    for task in todos:
-        if task.get("completed") is True:
-            completed.append(task.get("title"))
-    print("Employee {} is done with tasks({}/{})".
-          format(user.get("name"), len(completed), len(todos)))
-    for title in completed:
-        print("\t{}".format(title))
 
+    file_name = userID + ".csv"
+    with open(file_name, "w", newline="") as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+        for task in todos:
+            task_id = task["id"]
+            task_title = task["title"]
+            task_completed = str(task["completed"])
+            writer.writerow([userID, name, task_completed, task_title])
