f49d79936b5b3aaba048e47e4251b30eba87e175
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-05-25 15:44:05 +0200

    Commit

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -4,9 +4,10 @@ Script uses REST API for a given employee ID, returns information about
 his/her TODO list progress
 """
 
+import csv
 import requests
 from sys import argv
-import csv
+
 
 if __name__ == "__main__":
     userID = argv[1]
