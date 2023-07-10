36120e7479a95f1a8ea5864ae45c6e13b37827b0
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-07-10 16:18:14 +0200

    Commit

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""module contains function which uses reddit api"""
+import requests
+from collections import Counter
+
+
+def count_words(subreddit, word_list, after=None):
+    """Count words function"""
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {'User-Agent': 'Custom User Agent'}
+    params = {'limit': 100, 'after': after}
+
+    try:
+        response = requests.get(url, headers=headers, params=params, timeout=5)
+        response.raise_for_status()
+        data = response.json()
+        if 'data' in data and 'children' in data['data']:
+            posts = data['data']['children']
+            titles = [post['data']['title'] for post in posts]
+            word_counts = Counter()
+            for title in titles:
+                words = title.lower().split()
+                word_counts.update(words)
+            after = data['data']['after']
+            if after is not None:
+                word_counts.update(count_words(subreddit, word_list, after))
+            return word_counts
+        return Counter()
+    except (requests.exceptions.RequestException, ValueError):
+        return Counter()
+
+
+def print_word_counts(word_counts):
+    """Print words function"""
+    sorted_counts = sorted(word_counts.items(), key=lambda x: (-x[1], x[0]))
+    for word, count in sorted_counts:
+        print(f"{word}: {count}")
+
+
+def count_words_ptiny(subreddit, word_list):
+    """Count words"""
+    word_counts = count_words(subreddit, word_list)
+    print_word_counts(word_counts)
